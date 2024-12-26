04d5069fee7ddc4bc094aa1dc5c83b6b01dfbe82
KurejiMilan <milanrko55@gmail.com>
2024-12-26 14:38:34 +0100

    python script to calculate inertia tensor or updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # gripper_mitsubishi_HM
-Custom gripper model made for mitsubishi HM gripper. This gripper is used with igues rebel robotic arm for ESD master project.
+Custom gripper model made for mitsubishi HM gripper. This gripper is used with igues rebel robotic arm for [ESD master project](https://github.com/KurejiMilan/igus_6dof_robotic_arm).
+
+## Table of Contents
+1. [URDF explanation](#URDF_explanation)
+    1. [Gipper links](#Links_for_gripper)
+2. [tensor_inertia.py](#tensor_inertia.py)
+    1. [available geometry shapes](#available_geometry_shapes)
+    2. [missing geometry shapes](#missing_geometry_shapes)
+3. [Inertia Tensor Calculation Reference](#Inertia_Tensor_Calculation_Reference)
+4. [Contributors](#contributors)
+5. [Contacts](#contacts)
+## URDF_explanation
+Following link makes up the __Mitsubishi HM gripper__.
+### Links_for_gripper
+- base link (cylinder)
+- first link (box)
+- left finger base(box)
+- right finger base(box)
+- left finger(box)
+- right finger(box)
+
+## tensor_inertia.py
+This python script calculates the inertia tensor for objects. This is done to include in interia tag which is necessary for correct simulation.
+### available_geometry_shapes
+- [x] **Cylinder**
+- [x] **Box**
+
+### missing_geometry_shapes
+- [ ] *Sphare*
+
+## Inertia_Tensor_Calculation_Reference
+The reference calculation used for calculation of interia tensor can be found in the wiki page.
+[wiki link](https://en.wikipedia.org/wikiList_of_moments_of_inertia#List_of_3D_inertia_tensors)
+
+## Contributors
+- [@KurejiMilan](https://github.com/KurejiMilan)
+## Contacts
+[milan.rai.kos@gmail.com](mailto:milan.rai.kos@gmail.com)
---
+++ tensor_inertia.py
@@ -0,0 +1,90 @@
+import sys
+
+shape = "cylinder" 
+ixx = 0.0
+iyy = 0.0
+izz = 0.0
+
+# mass in kg 
+mass = 0.01
+radius = 0.039
+height = 0.006
+
+width = 0.0
+depth = 0.0
+
+def define_usage():
+    print(" Incorrect usage.\n")
+    print("tensor_inertia.py shape_name mass radius/width height depth.\n")
+    print("-----------------\n")
+    print("|  shape_name   |\n")
+    print("-----------------\n")
+    print("|'cylinder' 'c'|\n")
+    print("|  'box'    'b'|\n")
+    print("-----------------\n")
+
+
+def check_sys_argv():
+    if len(sys.argv)-1 == 0:
+        define_usage()
+        sys.exit()
+    elif len(sys.argv) -1 > 5:
+        define_usage()
+        sys.exit()
+    
+    # 
+    try:
+        float(sys.argv[1])
+        print("First argument should specify the shape.\n")
+        define_usage()
+        sys.exit()
+    except ValueError:
+        pass
+
+    # List to store the type converted dimesion
+    num_arg = []
+    # find the geometry shape
+    if sys.argv[1] == "cylinder" or sys.argv[1] == "c":
+        shape = "cylinder"
+        for i in range(2, 5):
+            try:
+                num_arg.append(float(sys.argv[i]))
+            except ValueError:
+                print("dimesnions should be in double.\n")
+                define_usage()
+                sys.exit()
+        mass = num_arg[0]
+        radius = num_arg[1]
+        height = num_arg[2]
+
+    elif  sys.argv[1] == "box" or sys.argv[1] == "b":
+        shape = "box"
+        for i in range(2, 6):
+            try:
+                num_arg.append(float(sys.argv[i]))
+            except ValueError:
+                print("dimesnions should be in double.\n")
+                define_usage()
+                sys.exit()
+        mass = num_arg[0]
+        width = num_arg[1]
+        height = num_arg[2]
+        depth = num_arg[3]
+    else :
+        print("passed geometry shape not defined.\n")
+        define_usage
+
+
+if __name__ == "__main__":
+    check_sys_argv()
+    if shape == "cylinder":
+        print("calculating tensor_inertia for solid cylinder.\n")
+        ixx = iyy = (mass*(3*pow(radius, 2)+pow(height,2)))/12
+        izz = (mass*pow(radius,2))/2
+    elif shape == "box":
+        print("calculating tensor_inertia for solid box.\n")
+        ixx = (mass*(pow(height, 2)+pow(depth,2)))/12
+        iyy = (mass*(pow(width, 2)+pow(height,2)))/12
+        izz = (mass*(pow(width, 2)+pow(depth,2)))/12
+
+    print('ixx="',ixx,'" ixy="',0.0,'" ixz="',0.0,'" iyy="',iyy,'" iyz="',0.0,'" izz="',izz,'"')
